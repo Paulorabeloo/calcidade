791ecd829b2d5ed5a372a67854a23c822da8d2fe
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-01-22 08:22:18 -0300

    calcidade

---
+++ atv2 - idade.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int anonasc, anoatual, idade;
+
+    anonasc = 2002;
+    anoatual = 2024;
+    idade = anoatual - anonasc;
+    printf("A idade atual é de: %i",idade);
+
+    return 0;
+}
